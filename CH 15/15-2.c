5de843aed1bf4eebdacd11be658af2e7629761d1
baldy343 <baldy34343@gmail.com>
2024-04-29 11:20:22 -0500

    starting chapter 15

---
+++ CH 15/15-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Write a program that calculates the product of a series of integers that are passed to function product using a variable-length argument list.
+//Test your function with several calls, each with a different number of arguments.
+
+int product(int n, ...)
+{
+    int result = 1;
+    va_list args;
+    va_start(args, n);
+    for (int i = 0; i < n; i++)
+    {
+        result *= va_arg(args, int);
+    }
+    va_end(args);
+    return result;
+}
